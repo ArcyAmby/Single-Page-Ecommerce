0d2149c41d998b8d300839f5f6690a3832391926
Arsenio Ambayec <arambayec@iskolarngbayan.pup.edu.ph>
2023-07-20 04:55:32 +0800

    config: tailwind minimal changes (colors)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
+    colors: {
+      customblack: '#0B0C10',
+      customDarkGray: '#1F2833',
+      customGray: '#C5C6C7',
+      customYellow: '#FFD95A',
+      customGold: '#C07F00'
+    },
     // screens: {
     //   'sm': '640px',
     //   // => @media (min-width: 640px) { ... }
@@ -22,13 +29,6 @@ module.exports = {
     //   // => @media (min-width: 1536px) { ... }
     // },
     extend: {
-      colors: {
-        customblack: '#0B0C10',
-        customDarkGray: '#1F2833',
-        customGray: '#C5C6C7',
-        customYellow: '#FFD95A',
-        customGold: '#C07F00'
-      },
       fontFamily: {
         primary: ['Montserrat']
       }
